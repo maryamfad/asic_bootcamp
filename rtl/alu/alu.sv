f8dad4ab4ffc9cc257e15db102a86c2321925052
maryamfad <maryamfadaee@gmail.com>
2026-08-06 23:36:21 -0400

    parameterize alu

--- rtl/alu/alu.sv
+++ rtl/alu/alu.sv
@@ -1,26 +1,26 @@
-module alu(
-    input logic [7:0] A,
-    input logic [7:0] B,
+module alu #( parameter WIDTH = 8)(
+    input logic [WIDTH-1:0] A,
+    input logic [WIDTH-1:0] B,
     input logic [2:0] op,
-    output logic [7:0] Y,
+    output logic [WIDTH-1:0] Y,
     output logic carry,
     output logic zero,
     output logic overflow
 );
-logic [8:0] result;
+logic [WIDTH:0] result;
 always_comb begin
 
-    Y = 8'b0;
-    carry = 1'b0;
-    overflow = 1'b0;
-    result = 9'b0;
-    
+    Y = '0;
+    carry = '0;
+    overflow = '0;
+    result = '0;
+
     case (op)
 
         3'b000: begin
             result = A+B;
-            Y=result[7:0];
-            carry=result[8];
+            Y=result[WIDTH-1:0];
+            carry=result[WIDTH];
         end
         3'b001:
             Y=A-B;  
@@ -41,7 +41,7 @@ always_comb begin
 
     endcase
 
-    zero = (Y==0);
+    zero = (Y=='0);
 end
 
 endmodule
