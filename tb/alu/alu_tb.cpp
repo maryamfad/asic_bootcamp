90e9d2665d254b6cf8e9e945506368e5e1074897
maryamfad <maryamfadaee@gmail.com>
2026-08-06 22:02:55 -0400

    implement a basic alu and tb for it

--- tb/alu/alu_tb.cpp
+++ tb/alu/alu_tb.cpp
@@ -3,6 +3,35 @@
 #include "verilated_vcd_c.h"
 #include <iostream>
 
+
+void check_result(
+        Valu* alu,
+        int A,
+        int B,
+        int op,
+        int expected,
+        const std:: string name
+    )
+    {
+        alu->A = A;
+        alu->B = B;
+        alu->op = op;
+
+        alu->eval();
+
+        if(alu->Y != expected){
+            std::cout << name << " FAILED. Expected"
+                        << expected
+                        << " Got "
+                        << (int)alu->Y
+                        << std::endl;
+        }
+        else{
+            std::cout << name << " PASSED\n";
+        }
+    }
+
+
 int main(int argc, char** argv)
 {
     Verilated::commandArgs(argc, argv);
@@ -10,7 +39,14 @@ int main(int argc, char** argv)
     Valu* alu=new Valu;
 
     int errors = 0;
-
+    constexpr int ADD = 0;
+    constexpr int SUB = 1;
+    constexpr int AND = 2;
+    constexpr int OR  = 3;
+    constexpr int XOR = 4;
+    constexpr int NOT = 5;
+    constexpr int SHL = 6;
+    constexpr int SHR = 7;
     Verilated::traceEverOn(true);
 
     VerilatedVcdC* tfp = new VerilatedVcdC;
@@ -19,43 +55,17 @@ int main(int argc, char** argv)
 
     tfp->open("waves/alu_wave.vcd");
 
-    //Test Add
-    alu->A=15;
-    alu->B=1;
-    alu->op=0b000;
+    
+    check_result(alu, 15, 1, ADD, 16, "ADD");
+    check_result(alu, 20, 5, SUB, 15, "SUB");
+    check_result(alu, 0b1100, 0b1010, AND, 0b1000, "AND");
+    check_result(alu, 0b1100, 0b1010, OR, 0b1110, "OR");
+    check_result(alu, 0b1100, 0b1010, XOR, 0b0110, "XOR");
+    check_result(alu, 0b00001111, 0, NOT, 0b11110000, "NOT");
+    check_result(alu, 0b00001111, 0, SHL, 0b00011110, "SHIFT LEFT");
+    check_result(alu, 0b00001110, 0, SHR, 0b00000111, "SHIFT RIGHT");
 
-    alu->eval();
-
-    if(alu->Y != 16)
-    {
-        std::cout << "ADD FAILED\n";
-        errors++;
-    }
-
-    //Test SUB
-    alu->A=20;
-    alu->B=3;
-    alu->op=0b001;
-
-    alu->eval();
-
-    if(alu->Y != 17){
-        std::cout << "SUB FAILED\n";
-        errors++;
-    }
-
-    //Test AND
-    alu->A=0b1100;
-    alu->B=0b1010;
-    alu->op = 0b010;
-
-    alu->eval();
-
-    if(alu->Y != 0b1000){
-        std::cout << "AND FAILED\n";
-        errors++;
-    }
-
+    
     tfp->close();
 
     if(errors == 0)
